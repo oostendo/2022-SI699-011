fe178fdde9651fd99be870ef6aada4bababc5321
Samantha Ryan-Lee <sryanlee@umich.edu>
2022-03-30 17:06:16 -0400

    basic app framework completed

--- eva_and_sam/flask/app.py
+++ eva_and_sam/flask/app.py
@@ -12,7 +12,7 @@ scatter_plot = alt.Chart(source).mark_circle(size=60).encode(
     color='Origin',
     tooltip=['Name', 'Origin', 'Horsepower', 'Miles_per_Gallon']
 ).interactive()
-scatter_plot.save('templates/scatter_plot.html')
+#scatter_plot.save('templates/scatter_plot.html')
 
 df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
                    'B': [5, 6, 7, 8, 9],
@@ -33,17 +33,17 @@ def index():
     return flask.render_template('index.html', charts=charts)
     # key is charts (referred to in the template), value is charts items from get_charts() function
 
-@app.route('/about')
-def about():
-    return flask.render_template('about.html')
+# @app.route('/about')
+# def about():
+#     return flask.render_template('about.html')
 
 @app.route('/scatter')
 def charts():
-    return flask.render_template('scatter_plot.html')
+    return flask.render_template('scatter_plot_test.html')
 
-@app.route('/table')
-def html_table():
-    return flask.render_template('table.html',  tables=[df.to_html(classes='data', index=False)], titles=df.columns.values)
+# @app.route('/table')
+# def html_table():
+#     return flask.render_template('table.html',  tables=[df.to_html(classes='data', index=False)], titles=df.columns.values)
 
 if __name__ == "__main__":
     app.run(debug=True)
